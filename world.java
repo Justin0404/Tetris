84b44aa5ec3d8d44a0bb8182c790bd76389d51d4
AndyWang45 <51407831+AndyWang45@users.noreply.github.com>
2019-06-12 08:54:22 -0400

    Add files via upload

---
+++ world.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class world here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class world extends World
+{
+
+    /**
+     * Constructor for objects of class world.
+     * 
+     */
+    public world()
+    {    
+        
+        super(10, 24, 30, true); 
+    }
+}
